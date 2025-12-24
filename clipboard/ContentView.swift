549efe59649dca496200be2d293285da5853c2a9
Mohamamed MAc <muhammadcivil96@gmail.com>
2025-12-24 14:35:05 +0300

    Initial Commit

---
+++ clipboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clipboard
+//
+//  Created by Haji Salam on 24/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clipboard/clipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardApp.swift
+//  clipboard
+//
+//  Created by Haji Salam on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct clipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
